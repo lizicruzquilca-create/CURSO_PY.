886a452be4aabe3daefc32206ab607ea0ec97116
lizicruzquilca-crear <lizicruzquilca@gamil.com>
2026-05-27 14:25:05 -0500

    AGREGANDO ACCESO A RANGO

---
+++ 09.DATOS_ESTRUCTURADOS/listas.py
@@ -0,0 +1,5 @@
+lista_vacia:list=[]
+print:list(len(lista_vacia)
+# por regla el nombre de la variable no debe tener el tipo de dato que se va almacenar
+amores:list[str]=['mama','papa','hermano']
+print(f"la cantidad de elementos que tiene la variable amores es: {len(amores)})
